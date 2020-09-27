29219e7fd3afede74ddde30cce41d4e055f67ae8
Ryan Tabar <70136507+SirFourier@users.noreply.github.com>
2020-09-27 23:29:30 +0100

    Network applied to MNIST and biases added

--- MyANN.py
+++ MyANN.py
@@ -1,19 +1,12 @@
-# My own Artificial Neural Network
-# This is a two-layer feed-forward neural network
-# A Neural Network is an interconnected system of perceptrons
-# Using sigmoid activation function
-
-# Import numpy for optimised matrix maths
+# My own Feed-Forward Artificial Neural Network
 import numpy as np
-
-# Import layer class
+import matplotlib.pyplot as plt
 from Layer import Layer
+from mnist import MNIST
 
 
-# Create a Feed Forward Neural Network class
 class NeuralNetwork:
-
-    # specify number of input, hidden nodes, output nodes, and hidden layers
+    # specify number of input nodes, hidden nodes, output nodes, and hidden layers
     def __init__(self, input_nodes, hidden_nodes, output_nodes, hidden_layers):
         # define layout (input layer + hidden layers + output layer)
         layout = [input_nodes] + [hidden_nodes for _ in range(hidden_layers)] + [output_nodes]
@@ -21,123 +14,129 @@ class NeuralNetwork:
         # create list of layers based on number of nodes current layer and the previous layer
         self.layers = [Layer(previous, current) for previous, current in zip(layout, layout[1:])]
 
-        # define derivative of activation function
-        self.act_prime = np.vectorize(lambda x: self.act(x) * (1 - self.act(x)))
-
-        # define derivative of cost function
-        self.cost_prime = np.vectorize(lambda x, y: 2 * (x - y))
-
     # define activation function (sigmoid)
     @staticmethod
     def act(x):
-        return 1 / (1 + np.exp(-x))
+        return 1. / (1. + np.exp(-x))
 
     # define derivative of activation function
     def act_prime(self, x):
-        return self.act(x) * (1 - self.act(x))
+        return self.act(x) * (1. - self.act(x))
 
     # define cost function
     @staticmethod
-    def cost_prime(target, prediction):
-        return 2 * (prediction - target)
+    def cost(target, prediction):
+        return 0.5 * np.sum((target - prediction) ** 2)
 
-    # define threshold of output
+    # define derivative of cost function
     @staticmethod
-    @np.vectorize
-    def threshold(x):
-        if x > 0.9:
-            return 1.0
-        elif x < 0.1:
-            return 0.0
-        else:
-            return 0
+    def cost_prime(target, prediction):
+        return prediction - target
 
-    # test
-    def test(self, inputs, targets, lr):
-        # Create random weights and biases for layer 1
-        W1 = np.random.normal(0.0, pow(2 / 2, -0.5), (3, 2))
-        b1 = np.full((3, 1), 0)
+    # perform feed forward
+    def feed_forward(self, input_layer):
+        # convert inputs to column vector and make it current layer
+        current_layer = input_layer.reshape(-1, 1)
 
-        # Create random weights and biases for layer 2
-        W2 = np.random.normal(0.0, pow(2 / 3, -0.5), (3, 3))
-        b2 = np.full((3, 1), 0)
+        # calculate activations through all the layers
+        for layer in self.layers:
+            current_layer = layer.calculate_act_outputs(current_layer, self.act)
 
-        # Create random weights and biases for layer 3
-        W3 = np.random.normal(0.0, pow(2 / 3, -0.5), (2, 3))
-        b3 = np.full((2, 1), 0)
+        # return final output layer
+        return current_layer
 
-        for _ in range(1000):
-            for i in range(4):
-                # Convert inputs list into numpy array
-                A0 = np.array(inputs[i]).reshape(-1, 1)
+    # train network
+    def train(self, inputs, targets, learn_rate, epochs):
+        # define number of training sets
+        n_sets = len(inputs)
 
-                # convert targets list into numpy array
-                Y = np.array(targets[i]).reshape(-1, 1)
+        # keep track of costs
+        cost_array = []
 
-                # Feed forward through each layer
-                Z1 = W1 @ A0 + b1
-                A1 = self.act(Z1)
+        # train network
+        for _ in range(epochs):
+            for i in range(n_sets):
+                # convert targets to column vector
+                Y = targets[i].reshape(-1, 1)
 
-                Z2 = W2 @ A1 + b2
-                A2 = self.act(Z2)
+                # feed forward
+                self.feed_forward(inputs[i])
 
-                Z3 = W3 @ A2 + b3
-                A3 = self.act(Z3)
+                # save cost
+                cost_array.append(self.cost(Y, self.layers[-1].act_outputs))
 
-                # calculate dJ_dW3
-                dJ_A3 = self.cost_prime(Y, A3)
-                dA3_dZ3 = self.act_prime(Z3)
-                dZ3_dW3 = A2.T
-                dJ_dW3 = (dJ_A3 * dA3_dZ3) @ dZ3_dW3
+                # calculate dJ_dW and dJ_dB of output layer
+                dJ_dZ = self.cost_prime(Y, self.layers[-1].act_outputs) * self.act_prime(self.layers[-1].outputs)
+                self.layers[-1].dJ_dW = dJ_dZ @ self.layers[-2].act_outputs.T
+                self.layers[-1].dJ_dB = dJ_dZ
 
-                # calculate dJ_dW2
-                dJ_dA2 = W3.T @ (dJ_A3 * dA3_dZ3)
-                dA2_dZ2 = self.act_prime(Z2)
-                dZ_dW2 = A1.T
-                dJ_dW2 = (dJ_dA2 * dA2_dZ2) @ dZ_dW2
+                # backpropagate up to but not including first layer and calculate dJ_dW and dJ_dB
+                for j in range(1, len(self.layers) - 1):
+                    dJ_dZ = (self.layers[-j].weights.T @ dJ_dZ) * self.act_prime(self.layers[-j-1].outputs)
+                    self.layers[-j-1].dJ_dW = dJ_dZ @ self.layers[-j-2].act_outputs.T
+                    self.layers[-j-1].dJ_dB = dJ_dZ
 
-                # calculate dJ_dW1
-                dJ_dA1 = W2.T @ (dJ_dA2 * dA2_dZ2)
-                dA1_dZ1 = self.act_prime(Z1)
-                dZ_dW1 = A0.T
-                dJ_dW1 = (dJ_dA1 * dA1_dZ1) @ dZ_dW1
+                # calculate dJ_dW and dJ_dB of first hidden layer
+                dJ_dZ = (self.layers[1].weights.T @ dJ_dZ) * self.act_prime(self.layers[0].outputs)
+                self.layers[0].dJ_dW = dJ_dZ @ inputs[i].reshape(1, -1)
+                self.layers[0].dJ_dB = dJ_dZ
 
-                # adjust all weights
-                W1 += lr * dJ_dW1
-                W2 += lr * dJ_dW2
-                W3 += lr * dJ_dW3
+                # adjust all weights and biases
+                for layer in self.layers:
+                    layer.weights -= learn_rate * layer.dJ_dW
+                    layer.biases -= learn_rate * layer.dJ_dB
 
-        for i in range(4):
-            # Convert inputs list into numpy array
-            A0 = np.array(inputs[i]).reshape(-1, 1)
-
-            # Feed forward through each layer
-            Z1 = W1 @ A0 + b1
-            A1 = self.act(Z1)
-
-            Z2 = W2 @ A1 + b2
-            A2 = self.act(Z2)
-
-            Z3 = W3 @ A2 + b3
-            A3 = self.act(Z3)
-
-            print(self.threshold(A3.T))
+        return cost_array
 
 
-# if this script is the main script executed
 if __name__ == "__main__":
-    # define Neural Network parameters
-    input_nodes = 2
-    hidden_nodes = 3
-    output_nodes = 2
-    hidden_layers = 1
+    # # load MNIST dataset
+    # mn_data = MNIST('./MNIST')
+    # mn_data.gz = True
+    # mn_images, mn_labels = mn_data.load_training()
+    #
+    # # normalise image data from 0-255 to 0.01 to 1
+    # images_array = (np.array(mn_images) / 255.0 * 0.99) + 0.01
+    #
+    # # convert labels into array of 10 elements with all zeros while the labeled element is replaced by 1
+    # labels_list = []
+    # for label in mn_labels:
+    #     empty = np.full(10, 0.01)
+    #     empty[label] = 0.99
+    #     labels_list.append(empty)
+    # labels_array = np.array(labels_list)
+    #
+    # # instantiate Neural Network
+    # my_ANN = NeuralNetwork(input_nodes=784, hidden_nodes=38, output_nodes=10, hidden_layers=2)
+    #
+    # # train network
+    # cost_data = my_ANN.train(images_array, labels_array, learn_rate=0.5, epochs=1)
+    # plt.plot(cost_data)
+    # plt.show()
+    #
+    # # test network
+    # mn_test_images, mn_test_labels = mn_data.load_testing()
+    # test_images_array = (np.array(mn_test_images) / 255.0 * 0.99) + 0.01
+    # test_labels_array = np.array(mn_test_labels)
+    #
+    # correct_labels = 0
+    # for index, test_image in enumerate(test_images_array):
+    #     guess = np.argmax(my_ANN.feed_forward(test_image))
+    #     if test_labels_array[index] == guess:
+    #         correct_labels += 1
+    # score = correct_labels / len(test_labels_array) * 100
+    # print(f"The network was {score}% correct on {len(test_labels_array)} test images!")
 
-    # create Neural Network object
-    my_ANN = NeuralNetwork(input_nodes, hidden_nodes, output_nodes, hidden_layers)
+    # instantiate Neural Network
+    my_ANN = NeuralNetwork(input_nodes=2, hidden_nodes=2, output_nodes=1, hidden_layers=1)
 
-    # train Network for an AND gate
-    inputs_list = [[0, 0], [0, 1], [1, 0], [1, 1]]
-    targets_list = [[1, 1], [1, 0], [0, 1], [0, 0]]
-    learning_rate = 0.5
+    inputs_array = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+    targets_array = np.array([0, 0, 0, 1])
 
-    my_ANN.test(inputs_list, targets_list, learning_rate)
+    cost_data = my_ANN.train(inputs_array, targets_array, learn_rate=1, epochs=500)
+
+    for inputs in inputs_array:
+        print(my_ANN.feed_forward(inputs))
+
+    plt.plot(cost_data)
+    plt.show()

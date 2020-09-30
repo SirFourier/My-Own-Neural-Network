530fdee57b04e3da3338d4893962d77d7e6cfb5e
Ryan Tabar <70136507+SirFourier@users.noreply.github.com>
2020-09-30 13:23:25 +0100

    added more comments

--- MyANN.py
+++ MyANN.py
@@ -1,17 +1,17 @@
-# My own Feed-Forward Artificial Neural Network
+# My own Feed-Forward Artificial Neural Network with variable number of hidden layers
 import numpy as np
-import matplotlib.pyplot as plt
 from Layer import Layer
 from mnist import MNIST
 
 
+# Create an artificial neural network
 class NeuralNetwork:
     # specify number of input nodes, hidden nodes, output nodes, and hidden layers
     def __init__(self, input_nodes, hidden_nodes, output_nodes, hidden_layers):
         # define layout (input layer + hidden layers + output layer)
         layout = [input_nodes] + [hidden_nodes for _ in range(hidden_layers)] + [output_nodes]
 
-        # create list of layers based on number of nodes current layer and the previous layer
+        # create list of layers based on number of nodes in current layer and the previous layer
         self.layers = [Layer(previous, current) for previous, current in zip(layout, layout[1:])]
 
     # define activation function (sigmoid)
@@ -19,16 +19,16 @@ class NeuralNetwork:
     def act(x):
         return 1. / (1. + np.exp(-x))
 
-    # define derivative of activation function
+    # define derivative of activation function (sigmoid prime)
     def act_prime(self, x):
         return self.act(x) * (1. - self.act(x))
 
-    # define cost function
+    # define cost function (outputs a single number: lower number means more accurate)
     @staticmethod
     def cost(target, prediction):
         return 0.5 * np.sum((target - prediction) ** 2)
 
-    # define derivative of cost function
+    # define derivative of cost function with respect to each prediction (our error function)
     @staticmethod
     def cost_prime(target, prediction):
         return prediction - target
@@ -50,9 +50,6 @@ class NeuralNetwork:
         # define number of training sets
         n_sets = len(inputs)
 
-        # keep track of costs
-        cost_array = []
-
         # train network
         for _ in range(epochs):
             for i in range(n_sets):
@@ -62,9 +59,7 @@ class NeuralNetwork:
                 # feed forward
                 self.feed_forward(inputs[i])
 
-                # save cost
-                cost_array.append(self.cost(Y, self.layers[-1].act_outputs))
-
+                # --------------------------------backpropagate with gradient descent------------------------------ #
                 # calculate dJ_dW and dJ_dB of output layer
                 dJ_dZ = self.cost_prime(Y, self.layers[-1].act_outputs) * self.act_prime(self.layers[-1].outputs)
                 self.layers[-1].dJ_dW = dJ_dZ @ self.layers[-2].act_outputs.T
@@ -86,10 +81,9 @@ class NeuralNetwork:
                     layer.weights -= learn_rate * layer.dJ_dW
                     layer.biases -= learn_rate * layer.dJ_dB
 
-        return cost_array
-
 
 if __name__ == "__main__":
+    # ----------------------------------------Handwritten Digits----------------------------------------------- #
     # load MNIST dataset
     mn_data = MNIST('./MNIST')
     mn_data.gz = True
@@ -110,35 +104,29 @@ if __name__ == "__main__":
     my_ANN = NeuralNetwork(input_nodes=784, hidden_nodes=38, output_nodes=10, hidden_layers=2)
 
     # train network
-    cost_data = my_ANN.train(images_array, labels_array, learn_rate=0.5, epochs=1)
+    my_ANN.train(images_array, labels_array, learn_rate=0.1, epochs=1)
 
     # test network
     mn_test_images, mn_test_labels = mn_data.load_testing()
     test_images_array = (np.array(mn_test_images) / 255.0 * 0.99) + 0.01
     test_labels_array = np.array(mn_test_labels)
-
     correct_labels = 0
-    for index, test_image in enumerate(test_images_array):
+    for test_label, test_image in zip(test_labels_array, test_images_array):
         guess = np.argmax(my_ANN.feed_forward(test_image))
-        if test_labels_array[index] == guess:
+        if test_label == guess:
             correct_labels += 1
-    score = correct_labels / len(test_labels_array) * 100
-    print(f"The network was {score}% correct on {len(test_labels_array)} test images!")
+    n_tests = len(test_labels_array)
+    score = correct_labels / n_tests * 100
+    print(f"The network was {score} % correct on {n_tests} test images!")
 
-    # plot cost data
-    plt.plot(cost_data)
-    plt.show()
-
+    # # -----------------------------------------XOR gate----------------------------------------------- #
     # # instantiate Neural Network
     # my_ANN = NeuralNetwork(input_nodes=2, hidden_nodes=2, output_nodes=1, hidden_layers=1)
     #
     # inputs_array = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
-    # targets_array = np.array([0, 0, 0, 1])
+    # targets_array = np.array([0, 1, 1, 0])
     #
-    # cost_data = my_ANN.train(inputs_array, targets_array, learn_rate=0.5, epochs=5000)
+    # my_ANN.train(inputs_array, targets_array, learn_rate=0.1, epochs=10000)
     #
     # for inputs in inputs_array:
     #     print(my_ANN.feed_forward(inputs))
-    #
-    # plt.plot(cost_data)
-    # plt.show()

507e2ace6be5ee17ee7e2283de6eb6347db5bc03
ryan-tabar <ryan.tabar321@gmail.com>
2023-01-06 14:47:38 +0000

    removed credentials

--- run_multiple_ANN.py
+++ run_multiple_ANN.py
@@ -9,11 +9,11 @@ learn_rate = 0.1
 epochs = 1
 
 changing_variable = "learning rate"
-max_iterations = 10
+max_iterations = 20
 
 if __name__ == "__main__":
     print(f"Training a sequence of ANNs with varying {changing_variable}...")
-    for learn_rate in range(1, max_iterations + 1):
+    for learn_rate in range(11, max_iterations + 1):
         print("\nStarting new training session.")
         learn_rate /= 10
         MyANN.handwritten_digits(input_nodes, hidden_nodes, output_nodes, hidden_layers, learn_rate, epochs)
